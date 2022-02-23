fcfea9cbf078027a91ffd4473edf9f0606a26aad
Paulo Santana <paulo.santana.r@gmail.com>
2022-02-23 08:24:27 -0300

    feat: init data inside main function

--- philo/main.c
+++ philo/main.c
@@ -1,6 +1,21 @@
-#include <unistd.h>
+#include "philosophers.h"
 
-int main(void)
+int	validate_args(int argc, char **argv)
 {
-	write(1, "hello world\n", 12);
+	(void)argv;
+	return (argc);
+}
+
+int main(int argc, char **argv)
+{
+	t_data	data;
+
+	if (validate_args(argc, argv) == 0)
+		return (1);
+	gettimeofday(&data.started_at, NULL);
+	data.num_philosophers = ft_atoi(argv[1]);
+	data.time_to_die = ft_atoi(argv[2]);
+	data.time_to_eat = ft_atoi(argv[3]);
+	data.time_to_sleep = ft_atoi(argv[4]);
+	data.max_meals = ft_atoi(argv[5]);
 }
