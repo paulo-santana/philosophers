2ed5b129b8987d41cef0e030b018e26a8b463911
Paulo Santana <paulo.santana.r@gmail.com>
2022-02-23 08:16:18 -0300

    feat: add philosopher and general data structs

--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: psergio- <psergio->                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 12:11:49 by psergio-          #+#    #+#             */
-/*   Updated: 2022/02/23 08:10:55 by psergio-         ###   ########.fr       */
+/*   Updated: 2022/02/23 08:15:41 by psergio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,21 @@
 # include <stdio.h>
 # include <sys/time.h>
 # include <stdlib.h>
+
+typedef struct s_data	t_data;
+typedef struct s_philo	t_philo;
+
+struct s_data {
+	unsigned int	time_to_die;
+	unsigned int	time_to_eat;
+	unsigned int	time_to_sleep;
+	long			started_at;
+};
+
+struct s_philo {
+	int		id;
+	t_data	*data;
+};
 
 int		ft_atoi(const char *str);
 
