699ab9dc878bdc9c2c24c84b1cde29cb5d50a5f0
Paulo Santana <paulo.santana.r@gmail.com>
2022-02-23 08:11:20 -0300

    feat: add function to get elapsed time in miliseconds

--- philo/time.c
+++ philo/time.c
@@ -1,24 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosophers.h                                     :+:      :+:    :+:   */
+/*   time.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: psergio- <psergio->                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/21 12:11:49 by psergio-          #+#    #+#             */
-/*   Updated: 2022/02/23 08:10:55 by psergio-         ###   ########.fr       */
+/*   Created: 2022/02/23 08:07:52 by psergio-          #+#    #+#             */
+/*   Updated: 2022/02/23 08:10:28 by psergio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILOSOPHERS_H
-# define PHILOSOPHERS_H
+#include "philosophers.h"
 
-# include <stdio.h>
-# include <sys/time.h>
-# include <stdlib.h>
+long	get_elapsed_time(struct timeval since)
+{
+	struct timeval	current_time;
 
-int		ft_atoi(const char *str);
-
-long	get_elapsed_time(struct timeval since);
-
-#endif /* !PHILOSOPHERS_H */
+	gettimeofday(&current_time, NULL);
+	return ((current_time.tv_sec - since.tv_sec) * 1000
+		+ (current_time.tv_usec - since.tv_usec) / 1000);
+}
